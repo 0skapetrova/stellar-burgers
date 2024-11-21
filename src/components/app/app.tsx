1a6e187d77a5e1dccd58c40467082f5bb2161a62
Olga Petrova <oskapetrovaa@yandex.ru>
2024-11-21 20:05:44 +0300

    first review

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,14 +1,109 @@
-import { ConstructorPage } from '@pages';
+import React, { useEffect } from 'react';
+import { Route, Routes, useLocation, useNavigate } from 'react-router-dom';
+
+import {
+  ConstructorPage,
+  ForgotPassword,
+  Login,
+  NotFound404,
+  Profile,
+  ProfileOrders,
+  Register,
+  ResetPassword
+} from '@pages';
+import { Feed } from '@pages';
 import '../../index.css';
 import styles from './app.module.css';
 
-import { AppHeader } from '@components';
+import { AppHeader, IngredientDetails, OrderInfo } from '@components';
+import { Modal } from '@components';
+import { OnlyAuth, OnlyUnAuth } from '../ProtectedRoute';
+import { useDispatch, useSelector } from '../../services/store';
+import { checkUserAuth } from '../../services/features/userSlice';
+import { getIngredientsData } from '../../services/features/ingredientsSlice';
+import { getOrdersData } from '../../services/features/ordersSlice';
+import { getFeedsData } from '../../services/features/feedsSlice';
 
-const App = () => (
-  <div className={styles.app}>
-    <AppHeader />
-    <ConstructorPage />
-  </div>
-);
+const App = () => {
+  const location = useLocation();
+
+  const backgroundLocation = location.state?.background;
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const onClose = () => {
+    navigate(-1);
+  };
+
+  useEffect(() => {
+    dispatch(checkUserAuth());
+    dispatch(getIngredientsData());
+    // dispatch(getOrdersData());
+    // dispatch(getFeedsData());
+  }, []);
+
+  return (
+    <div className={styles.app}>
+      <AppHeader />
+      <Routes location={backgroundLocation || location}>
+        <Route path='/' element={<ConstructorPage />} />
+        <Route path='/feed' element={<Feed />} />
+        <Route path='/login' element={<OnlyUnAuth component={<Login />} />} />
+        <Route
+          path='/register'
+          element={<OnlyUnAuth component={<Register />} />}
+        />
+        <Route
+          path='/forgot-password'
+          element={<OnlyUnAuth component={<ForgotPassword />} />}
+        />
+        <Route
+          path='/reset-password'
+          element={<OnlyUnAuth component={<ResetPassword />} />}
+        />
+        <Route path='/profile' element={<OnlyAuth component={<Profile />} />} />
+        <Route
+          path='/profile/orders'
+          element={<OnlyAuth component={<ProfileOrders />} />}
+        />
+        <Route path='*' element={<NotFound404 />} />
+      </Routes>
+
+      {backgroundLocation && (
+        <Routes>
+          <Route
+            path='/feed/:number'
+            element={
+              <Modal title='' onClose={onClose}>
+                <OrderInfo />
+              </Modal>
+            }
+          />
+          <Route
+            path='/ingredients/:id'
+            element={
+              <Modal title='' onClose={onClose}>
+                <IngredientDetails />
+              </Modal>
+            }
+          />
+          <Route
+            path='/profile/orders/:number'
+            element={
+              <OnlyAuth
+                component={
+                  <Modal title='' onClose={onClose}>
+                    <OrderInfo />
+                  </Modal>
+                }
+              />
+            }
+          />
+        </Routes>
+      )}
+    </div>
+  );
+};
 
 export default App;
--- src/services/store.ts
+++ src/services/store.ts
@@ -1,12 +1,25 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineSlices, configureStore } from '@reduxjs/toolkit';
 
 import {
   TypedUseSelectorHook,
   useDispatch as dispatchHook,
   useSelector as selectorHook
 } from 'react-redux';
+import { userSlice } from './features/userSlice';
+import { ingredientsSlice } from './features/ingredientsSlice';
+import { constructorSlice } from './features/constructorSlice';
+import { orderSlice } from './features/orderSlice';
+import { ordersSlice } from './features/ordersSlice';
+import { feedSlice } from './features/feedsSlice';
 
-const rootReducer = () => {}; // Заменить на импорт настоящего редьюсера
+export const rootReducer = combineSlices(
+  userSlice,
+  ingredientsSlice,
+  constructorSlice,
+  orderSlice,
+  ordersSlice,
+  feedSlice
+);
 
 const store = configureStore({
   reducer: rootReducer,
